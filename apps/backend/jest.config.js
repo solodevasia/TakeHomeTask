d613662ced5281f755cf4c64660b6ce6be4ee2f5
Kenedy Nopriansyah <kenedynopriansyah@Kenedys-MacBook-Air.local>
2024-11-22 22:47:52 +0700

    feat: 🎸 setup workspace:apps:backend

---
+++ apps/backend/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['src'],
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: ['src/main.ts', 'src/app.module.ts'],
+};
