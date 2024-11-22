fee21b78b50ccb1c6ac7781839584226ab06c30a
Kenedy Nopriansyah <kenedynopriansyah@Kenedys-MacBook-Air.local>
2024-11-22 22:25:34 +0700

    feat: 🎸 setup

---
+++ eslint.base.js
@@ -0,0 +1,13 @@
+const eslint = require("@eslint/js");
+const tsESLINT = require("typescript-eslint");
+const importESLINT = require("eslint-plugin-import/config/typescript");
+const prettierESLINT = require("eslint-plugin-prettier/recommended");
+
+const config = tsESLINT.config(
+  eslint.configs.recommended,
+  ...tsESLINT.configs.recommended,
+  prettierESLINT,
+  importESLINT
+);
+
+module.exports = { config, tsESLINT };
